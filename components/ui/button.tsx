c3ada7d8483db5fa70d3d7bfe63066e1ee1748a4
duonganh203 <rankill52@gmail.com>
2024-05-12 11:26:32 +0700

    ui: config variant and size

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,20 +5,20 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 rounded-full",
     {
         variants: {
             variant: {
                 default: "hover:bg-black hover:text-white",
                 primary:
                     "bg-black text-white hover:bg-white hover:text-[#303033] ring-[1px] ring-black",
+                primaryOutline: "text-black border border-black",
             },
             size: {
-                default: "h-4 px-2 rounded-full py-2",
-                sm: "h-6 rounded-full px-3",
-                lg: "h-8 rounded-full px-5",
-                icon: "h-10 w-10",
-                rounded: "rounded-full",
+                default: "h-4 px-2 py-2",
+                sm: "h-8 px-5",
+                lg: "h-10 px-7",
+                icon: "h-8 w-8",
             },
         },
         defaultVariants: {
