51e877cebba2cd6c74e4f1e3dc71bcd552f76f08
quanghung309 <95295846+quanghung309@users.noreply.github.com>
2024-05-16 03:13:36 +0700

    #10 UI: Signup Form

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { FeatureSticker } from "./feature-sticker";
 type Props = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 const AuthLayout = ({ children }: Props) => {
-    return (
-        <div className='w-full h-[804px] flex'>
-            <div className='w-[50%]'>{children}</div>
-            <FeatureSticker />
-        </div>
-    );
+  return (
+    <div className="w-full h-[804px] flex">
+      <div className="w-[50%] h-full flex items-center justify-end">
+        {children}
+      </div>
+      <FeatureSticker />
+    </div>
+  );
 };
 
 export default AuthLayout;
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,9 +1,35 @@
 import { z } from "zod";
 
 export const LoginSchema = z.object({
+  email: z.string().email({ message: "Please enter valid email address!" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long!" }),
+  isRemember: z.boolean(),
+});
+
+export const SignUpSchema = z
+  .object({
+    firstName: z.string({
+      message: "Please enter your first name!",
+    }),
+    lastName: z.string({
+      message: "Please enter your last name!",
+    }),
     email: z.string().email({ message: "Please enter valid email address!" }),
     password: z
-        .string()
-        .min(6, { message: "Password must be at least 6 characters long!" }),
-    isRemember: z.boolean(),
-});
+      .string()
+      .min(6, { message: "Password must be at least 6 characters long!" }),
+    confirmPassword: z.string().min(6),
+
+    isSendMail: z.boolean(),
+  })
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "The passwords did not match",
+        path: ["confirmPassword"],
+      });
+    }
+  });
