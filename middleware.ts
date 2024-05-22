734e3f44b6ec9cc7b94b19a2e1b5b78e5f2d955e
Duong Anh <rankill52@gmail.com>
2024-05-22 11:41:20 +0700

    #17 ui: add variant cancel button

--- middleware.ts
+++ middleware.ts
@@ -3,25 +3,35 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 const privatePaths = [
-  "/my-fitness-journey",
-  "/settings/preferences",
-  "/email-and-password",
-  "/subscription",
+    "/my-fitness-journey",
+    "/settings/preferences",
+    "/email-and-password",
+    "/subscription",
 ];
 const authPaths = ["/login", "/signup"];
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  const sessionToken = cookies().get("sessionToken")?.value;
-  if (privatePaths.some((path) => pathname.startsWith(path)) && !sessionToken) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-  if (authPaths.some((path) => pathname.startsWith(path)) && sessionToken)
-    return NextResponse.redirect(new URL("/program", request.url));
-  return NextResponse.next();
+    const { pathname } = request.nextUrl;
+    const sessionToken = cookies().get("sessionToken")?.value;
+    if (
+        privatePaths.some((path) => pathname.startsWith(path)) &&
+        !sessionToken
+    ) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+    if (authPaths.some((path) => pathname.startsWith(path)) && sessionToken)
+        return NextResponse.redirect(new URL("/program", request.url));
+    return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: [...privatePaths, ...authPaths],
+    matcher: [
+        "/my-fitness-journey",
+        "/settings/preferences",
+        "/email-and-password",
+        "/subscription",
+        "/login",
+        "/signup",
+    ],
 };
