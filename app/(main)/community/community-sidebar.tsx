bd97a8996ee4a4df15cd2a84ab94f73417db0ed8
Duong Anh <rankill52@gmail.com>
2024-05-27 08:35:01 +0700

    #3 ui: fix config sidebar

--- app/(main)/community/community-sidebar.tsx
+++ app/(main)/community/community-sidebar.tsx
@@ -8,10 +8,10 @@ import React from "react";
 const CommunitySideBar = () => {
     const pathname = usePathname();
     return (
-        <div className=" flex fixed top-[110px] left-0 min-w-[333px] flex-col border-r border-gray-200">
-            <div className="flex size-full  flex-col h-screen gap-4 ">
-                <nav className="flex-col h-[850px] justify-between md:flex md:gap-4 ">
-                    <ul className=" w-full flex-col items-start gap-2 md:flex">
+        <div className=' flex fixed top-[110px] left-0 min-w-[333px] flex-col border-r border-gray-200 h-[calc(100vh-110px)] z-20'>
+            <div className='flex size-full flex-col gap-4 '>
+                <nav className='flex-col md:flex md:gap-4 h-full'>
+                    <ul className='w-full flex-col items-start gap-2 flex flex-1'>
                         {communityNavLink.map((link) => {
                             const isActive = link.route === pathname;
                             return (
@@ -24,14 +24,14 @@ const CommunitySideBar = () => {
                                     }`}
                                 >
                                     <Link
-                                        className="flex size-full gap-4 p-4 items-center"
+                                        className='flex size-full gap-4 p-4 items-center'
                                         href={link.route}
                                     >
                                         <Image
                                             src={link.icon}
                                             width={24}
                                             height={24}
-                                            alt="logo"
+                                            alt='logo'
                                             className={`${
                                                 isActive && "brightness-600"
                                             }`}
@@ -42,11 +42,12 @@ const CommunitySideBar = () => {
                             );
                         })}
                     </ul>
-                    <ul className="flex w-full items-center h-[64.7px] py-3 border-t border-gray-200 relative">
+                    <ul className='flex w-full items-center h-[64.7px] py-3 border-t border-gray-200 relative'>
                         {communityNavBack.map((link) => {
                             const isActives = link.route === pathname;
                             return (
                                 <li
+                                    key={link.route}
                                     className={`w-[25%] h-full flex items-center justify-center group ${
                                         isActives
                                             ? "rounded-full bg-[#EDEBF9]"
@@ -58,13 +59,13 @@ const CommunitySideBar = () => {
                                             src={link.icon}
                                             width={24}
                                             height={24}
-                                            alt="logo"
+                                            alt='logo'
                                             className={`${
                                                 isActives && " text-blue-500"
                                             }`}
                                         ></Image>
                                     </Link>
-                                    <div className="hidden hover:bg-black hover:flex hover:text-white hover: width-full">
+                                    <div className='hidden hover:bg-black hover:flex hover:text-white hover: width-full'>
                                         {link.label}
                                     </div>
                                 </li>
--- app/(main)/community/layout.tsx
+++ app/(main)/community/layout.tsx
@@ -9,15 +9,13 @@ type Props = {
 
 const layout = ({ children }: Props) => {
     return (
-        <div className=" mt-[1.5px] w-full">
+        <div className=' mt-[1.5px] w-full'>
             <CommunityNavbar />
-
-            <div className="bg-[#F9F9FA] flex">
+            <div className='bg-[#F9F9FA] flex'>
                 <CommunitySideBar />
-
-                <div className="flex flex-col w-full ml-[213px] ">
+                <div className='flex flex-col w-full ml-[213px] '>
                     <CommunityFilter />
-                    <div className=" ml-[333px]">{children}</div>
+                    <div className=' ml-[333px]'>{children}</div>
                 </div>
             </div>
             <div></div>
